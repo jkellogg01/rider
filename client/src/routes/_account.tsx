3b865f0f618f5cd3c67d49111df7f56624ace40a
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-09-25 19:40:32 -0600

    obsessing over layouts

--- client/src/routes/_account.tsx
+++ client/src/routes/_account.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_account")({
 	component: AccountPage,
@@ -7,8 +7,15 @@ export const Route = createFileRoute("/_account")({
 function AccountPage() {
 	// TODO: make a real page here
 	return (
-		<div className="h-dvh content-center mx-4">
-			<Outlet />
-		</div>
+		< div className="min-h-dvh flex flex-col" >
+			{/* HACK: don't like that I need flex for this but h-full doesn't seem to work right */}
+			<div className="container mx-auto p-2">
+				<Link to="/" className="block font-bold text-xl">Rider</Link>
+			</div>
+			<hr />
+			<div className="h-full flex-grow content-center">
+				<Outlet />
+			</div>
+		</div >
 	);
 }
