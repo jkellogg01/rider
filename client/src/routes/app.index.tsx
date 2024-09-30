2ae7d51ee656c2c18fbc792063fdb64813307f67
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-09-30 10:16:40 -0600

    little bit of message preview shenans
    im really just procrastinating on setting up a mail server, i would low
    key rather eat shards of glass but it must be done

--- client/src/routes/app.index.tsx
+++ client/src/routes/app.index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
 	Card,
@@ -38,6 +37,14 @@ function AppIndex() {
 	return (
 		<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
 			<div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+				<Card>
+					<CardHeader>
+						<CardTitle>Quick Actions</CardTitle>
+					</CardHeader>
+					<CardContent className="grid gap-8">
+						There aren't any actions yet.
+					</CardContent>
+				</Card>
 				<Card className="xl:col-span-2">
 					<CardHeader className="flex flex-row items-center">
 						<div className="grid gap-2">
@@ -66,7 +73,7 @@ function AppIndex() {
 												{email}
 											</div>
 										</TableCell>
-										<TableCell>{body}</TableCell>
+										<TableCell>{preview(body)}</TableCell>
 										<TableCell>
 											<Button variant="outline" size="sm">
 												View
@@ -78,81 +85,27 @@ function AppIndex() {
 						</Table>
 					</CardContent>
 				</Card>
-				<Card x-chunk="dashboard-01-chunk-5">
-					<CardHeader>
-						<CardTitle>Recent Sales</CardTitle>
-					</CardHeader>
-					<CardContent className="grid gap-8">
-						<div className="flex items-center gap-4">
-							<Avatar className="hidden h-9 w-9 sm:flex">
-								<AvatarImage src="/avatars/01.png" alt="Avatar" />
-								<AvatarFallback>OM</AvatarFallback>
-							</Avatar>
-							<div className="grid gap-1">
-								<p className="text-sm font-medium leading-none">
-									Olivia Martin
-								</p>
-								<p className="text-sm text-muted-foreground">
-									olivia.martin@email.com
-								</p>
-							</div>
-							<div className="ml-auto font-medium">+$1,999.00</div>
-						</div>
-						<div className="flex items-center gap-4">
-							<Avatar className="hidden h-9 w-9 sm:flex">
-								<AvatarImage src="/avatars/02.png" alt="Avatar" />
-								<AvatarFallback>JL</AvatarFallback>
-							</Avatar>
-							<div className="grid gap-1">
-								<p className="text-sm font-medium leading-none">Jackson Lee</p>
-								<p className="text-sm text-muted-foreground">
-									jackson.lee@email.com
-								</p>
-							</div>
-							<div className="ml-auto font-medium">+$39.00</div>
-						</div>
-						<div className="flex items-center gap-4">
-							<Avatar className="hidden h-9 w-9 sm:flex">
-								<AvatarImage src="/avatars/03.png" alt="Avatar" />
-								<AvatarFallback>IN</AvatarFallback>
-							</Avatar>
-							<div className="grid gap-1">
-								<p className="text-sm font-medium leading-none">
-									Isabella Nguyen
-								</p>
-								<p className="text-sm text-muted-foreground">
-									isabella.nguyen@email.com
-								</p>
-							</div>
-							<div className="ml-auto font-medium">+$299.00</div>
-						</div>
-						<div className="flex items-center gap-4">
-							<Avatar className="hidden h-9 w-9 sm:flex">
-								<AvatarImage src="/avatars/04.png" alt="Avatar" />
-								<AvatarFallback>WK</AvatarFallback>
-							</Avatar>
-							<div className="grid gap-1">
-								<p className="text-sm font-medium leading-none">William Kim</p>
-								<p className="text-sm text-muted-foreground">will@email.com</p>
-							</div>
-							<div className="ml-auto font-medium">+$99.00</div>
-						</div>
-						<div className="flex items-center gap-4">
-							<Avatar className="hidden h-9 w-9 sm:flex">
-								<AvatarImage src="/avatars/05.png" alt="Avatar" />
-								<AvatarFallback>SD</AvatarFallback>
-							</Avatar>
-							<div className="grid gap-1">
-								<p className="text-sm font-medium leading-none">Sofia Davis</p>
-								<p className="text-sm text-muted-foreground">
-									sofia.davis@email.com
-								</p>
-							</div>
-							<div className="ml-auto font-medium">+$39.00</div>
-						</div>
-					</CardContent>
-				</Card>
 			</div>
 		</main>
 	);
+}
+
+function preview(body: string) {
+	if (body.length < 65) {
+		return body;
+	}
+
+	const words = body.split(" ");
+	let idx = 0;
+	let sum = 0;
+	while (true) {
+		sum += words[idx].length;
+		if (sum > 62) {
+			break;
+		}
+		// compensate for the space
+		sum++;
+		idx++;
+	}
+	return words.slice(0, idx).join(" ").concat("...");
 }
