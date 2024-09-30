b920febcae22e3cf3631f88ce50107d5ba053901
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-09-30 16:45:05 -0600

    get bands by user

--- server/handler/band.go
+++ server/handler/band.go
@@ -1,1 +1,28 @@
 package handler
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+)
+
+func (cfg *config) GetUserBands(w http.ResponseWriter, r *http.Request) {
+	id, ok := r.Context().Value("current-user").(int32)
+	if !ok {
+		RespondWithError(w, http.StatusBadRequest, "invalid or missing user id")
+		return
+	}
+
+	bands, err := cfg.db.GetAccountBands(r.Context(), id)
+	if errors.Is(err, sql.ErrNoRows) {
+		RespondWithError(w, http.StatusNotFound, "no bands for this account")
+		return
+	} else if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, "unexpected database error")
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, map[string]any{
+		"bands": bands,
+	})
+}
