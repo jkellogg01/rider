4f963206d941ae51703ef81a3ab164d3a4330318
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-02 15:13:37 -0600

    bit of a sql syntax error perhaps

--- server/database/bands.sql.go
+++ server/database/bands.sql.go
@@ -57,7 +57,7 @@ func (q *Queries) CreateBand(ctx context.Context, name string) (Band, error) {
 const getAccountBands = `-- name: GetAccountBands :many
 select ab.account_id, ab.account_is_admin, ab.created_at as joined_at, ab.updated_at as join_updated_at, b.id, b.name, b.created_at, b.updated_at from account_band ab
 join band b
-on account_id = $1 and band_id = band.id
+on ab.account_id = $1 and ab.band_id = b.id
 `
 
 type GetAccountBandsRow struct {
--- server/sql/queries/bands.sql
+++ server/sql/queries/bands.sql
@@ -1,7 +1,7 @@
 -- name: GetAccountBands :many
 select ab.account_id, ab.account_is_admin, ab.created_at as joined_at, ab.updated_at as join_updated_at, b.id, b.name, b.created_at, b.updated_at from account_band ab
 join band b
-on account_id = $1 and band_id = band.id;
+on ab.account_id = $1 and ab.band_id = b.id;
 
 -- name: CreateBand :one
 insert into band (name) values ($1) returning *;
