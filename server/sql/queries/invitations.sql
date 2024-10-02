b5bdcfafb105fd45d2a3217ec9b7be458d916891
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-02 16:54:58 -0600

    should be all the queries for invitations

--- server/sql/queries/invitations.sql
+++ server/sql/queries/invitations.sql
@@ -3,3 +3,10 @@ SELECT * FROM invitation
 WHERE body = $1
 GROUP BY id
 LIMIT 1;
+
+-- name: CreateInvitation :one
+INSERT INTO invitation (
+  creator_id, band_id, body, expires_at,
+) VALUES (
+  $1, $2, $3, $4
+) RETURNING *;
--- server/sql/schema/003_invite.sql
+++ server/sql/schema/003_invite.sql
@@ -2,10 +2,10 @@
 CREATE TABLE invitation (
   id serial PRIMARY KEY,
   body text UNIQUE NOT NULL,
-  creator_id int NOT NULL REFERENCES account.id,
-  band_id int NOT NULL REFERENCES band.id,
+  creator_id int NOT NULL REFERENCES account (id),
+  band_id int NOT NULL REFERENCES band (id),
   created_at timestamp NOT NULL DEFAULT NOW(),
-  expires_at timestamp NOT NULL,
+  expires_at timestamp NOT NULL
 );
 
 -- +goose Down
